23ec4c750ccfcbbb6cb8e32eb0f62476bb9fe859
SUL 2 - LUIZ GONZAGA PINTO E SILVA PROFESSOR <malvaoraphael0@gmail.com>
2024-09-02 17:17:09 -0300

    test

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
+
+
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
-# Create your views here.
+    posts = Post.objects.all() 
+    return render(request, 'blog/post_list.html', {'posts': posts})
